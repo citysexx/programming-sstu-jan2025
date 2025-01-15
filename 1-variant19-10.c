135b564ba8c15094cd372180b2dba6242122c315
Kodotochka <kodotochka@gmail.com>
2025-01-15 15:34:20 +0400

    test

---
+++ 1-variant19-10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+  double a, x;
+  printf("Input your a number (a) >>> ");
+  scanf("%lf", &a);
+  printf("Answer >>> %lf\n", x = pow(sin(a/2), 3));
+  return 0;
+}
